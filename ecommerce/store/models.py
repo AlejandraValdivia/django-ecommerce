e1d2f3bc8b6a358c7bb39f96d26ad50cf2cc5087
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-12 16:15:10 -0800

    feat: populate database and add images

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -16,6 +16,8 @@ class Category(models.Model):
     
 
 class Product(models.Model):
+    # Foreigkey
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True)
 
     title = models.CharField(max_length=250)
 
