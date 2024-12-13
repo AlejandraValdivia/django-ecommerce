f92e09806b1da1fbc100573977c52f7e3f3766f4
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-12 18:23:07 -0800

    feat: create single product page

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-# app_name = 'store'
-
 urlpatterns = [
     path('', views.store, name='store'),
-    # path('category/<slug:slug>', views.category, name='category'),
-    # path('product/<slug:slug>', views.product, name='product'),
+    path('product/<slug:slug>/', views.product_info, name='product-info'),
+
 ]
