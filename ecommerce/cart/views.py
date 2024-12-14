a50f9d6fbe6c7d99edd3c17bdf75e6f37a062a8f
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-14 13:55:34 -0800

    feat: add basic urls and views for our cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart_summary(request):
+    # return render(request, 'cart/summary.html')
+    pass
+
+def cart_add(request):
+    # return render(request, 'cart/add.html')
+    pass
+
+def cart_delete(request):
+    # return render(request, 'cart/delete.html')
+    pass
+
+def cart_update(request):
+    # return render(request, 'cart/update.html')
+    pass
