6bfaaabfcee00c7d3472957d9e443df135597e10
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-19 18:53:27 -0800

    feat: configure account url files

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -23,9 +23,18 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+
+    # Admin site URL
     path('admin/', admin.site.urls),
+
+    # Store app
     path('', include('store.urls')),
+
+    # Cart app
     path('cart/', include('cart.urls')),
+
+    # Account app
+    path('account/', include('account.urls')),
     
 ]
 
--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.store, name='store'),
     path('product/<slug:product_slug>/', views.product_info, name='product-info'),
     path('search/<slug:category_slug>/', views.list_category, name='list-category'),
-
 
 ]
