fc60f1201c9a6a7b0c8a31f59759d5644d9f636a
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-19 18:26:28 -0800

    feat: add update and delete cart functionality

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -17,22 +17,48 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
+
         product = get_object_or_404(Product, id=product_id)
 
         cart.add(product=product, product_qty=product_quantity)
-    # if product is not None and product_quantity > 0:
-    #     cart.add(product=product, product_qty=product_quantity)
 
         cart_quantity = cart.__len__()
 
         response = JsonResponse({'qty': cart_quantity})
+
         return response
     
 
 def cart_delete(request):
-    # return render(request, 'cart/delete.html')
-    pass
+
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({'qty': cart_quantity, 'total': cart_total})
+        return response
+        
+
 
 def cart_update(request):
-    # return render(request, 'cart/update.html')
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        cart.update(product=product_id, qty=product_quantity)
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+        response = JsonResponse({'qty': cart_quantity, 'total': cart_total})
+        
+        return response
+    
