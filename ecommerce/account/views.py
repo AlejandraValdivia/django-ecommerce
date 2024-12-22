97ec1c2f0226e7a15d3157356552da1f588fb2aa
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-22 14:12:31 -0800

    feat: email verification message view setup

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import redirect, render
 
 from .forms import CreateUserForm
 
-# Create your views here.
+from django.contrib.sites.shortcuts import get_current_site
+from . token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+
+
 
 def register(request):
     
@@ -11,9 +19,39 @@ def register(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('store')
+            user = form.save()
+            user.is_active = False
+            user.save()
+
+            # Email verification setup (template)
+
+            current_site = get_current_site(request)
+            subject = 'Account verification email'
+
+            message = render_to_string('account/registration/email-verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user)
+            })  # this message will be sent to the user's email address
+
+            user.email_user(subject, message)
+
+            return redirect('email-verification-sent')
 
     context = {'form': form}
 
     return render(request, 'account/registration/register.html', context=context)
+
+
+def email_verification(request):
+    pass
+
+def email_verification_sent(request):
+    pass
+
+def email_verification_success(request):
+    pass
+
+def email_verification_failed(request):
+    pass
