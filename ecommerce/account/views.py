8b1163fec8a0d1da4a03633e2e7d72bda20644c6
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-29 17:07:54 -0800

    feat: add manage-shipping view and url

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 
 from .forms import CreateUserForm, LoginForm, UpdateUserForm
 
+from payment.forms import ShippingForm
+
+from payment.models import ShippingAddress
+
 from django.contrib.auth.models import User
 
 from django.contrib.sites.shortcuts import get_current_site
@@ -148,4 +152,25 @@ def delete_account(request):
         return redirect('store')
     return render(request, 'account/delete-account.html')
 
+@login_required(login_url='my-login')
+def manage_shipping(request):
 
+    try: 
+        shipping = ShippingAddress.objects.get(user=request.user.id)
+    except ShippingAddress.DoesNotExist:
+        shipping = None
+
+    form = ShippingForm(instance=shipping)
+
+    if request.method == 'POST':
+        form = ShippingForm(request.POST, instance=shipping)
+        if form.is_valid():
+            shipping_user = form.save(commit=False)
+            shipping_user.user = request.user
+            shipping_user.save()
+            
+            return redirect('dashboard')
+       
+    context = {'form': form}
+    
+    return render(request, 'account/manage-shipping.html', context=context)
